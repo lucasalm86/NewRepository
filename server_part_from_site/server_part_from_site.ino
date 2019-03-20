4147b73d8509305730074b02927dc4c20c4b1e93
lucas <lucas@gmail.com>
2019-03-20 15:47:39 +0000

    develop version
    Необходимо переработать подключение к сети: выбор между режимами сети

--- server_part_from_site/server_part_from_site.ino
+++ server_part_from_site/server_part_from_site.ino
@@ -2,6 +2,8 @@
 
 #include<EthernetClient.h> //Uncomment this library to work with ESP8266
 #include<ESP8266WiFi.h> //Uncomment this library to work with ESP8266
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
 
 //объявление параметров для датчиков
 //---------------------------------------------------------------------
@@ -11,11 +13,17 @@
 float p10, p25;
 #define pwmPin D8 // пин для приема уровня СО2
 
-SDS011 my_sds; 
+SDS011 my_sds;
+bool my_sds_onoff = false;
 
 long th, tl, h, l, ppm;
 int error;
 
+
+ESP8266WebServer _server(80);
+String ssidAP, passwordAP, content, ssid_login, ssid_password, passwordChanged, adminpasswordChanged, modeAP_RP_global, globalRP_IP, Co2_status, Co2_text_color, esid, epass;
+String st, standartStrHtml, standartStrHtmlEnd;
+bool clearEepromHttpAnswer, connectedToWiFiSsid, globalIP_status_Connection;
 //примитивный подсчет времени
 //---------------------------------------------------------------------
 int indDelaySecs;
@@ -26,9 +34,9 @@ int indDelayWork = 0;
 
 char jsonBuffer[500] = "["; // Initialize the jsonBuffer to hold data
 
-char ssid[] = "D-Link_I"; //  Your network SSID (name)
+char ssid[] = "Xia_n2"; //  Your network SSID (name)
 
-char pass[] = "125896347gerda"; // Your network password
+char pass[] = "Envi_32Envi_32"; // Your network password
 WiFiClient client; // Initialize the WiFi client library
 
 char server[] = "api.thingspeak.com"; // ThingSpeak Server
@@ -45,10 +53,40 @@ String Co2, PM2_5, PM10, Temp, Hum, Pres, valueField, fieldVALUE;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+
+  EEPROM.begin(512);
+  Serial.print("Writing EEPROM... ");
+  //writeEEPROM("admin", "admin", "Xia_n2", "Envi_32Envi_32", "airDeviceTester", "Password", "AP");
+
   Wire.begin();
-  my_sds.begin(D6, D7); // begin(uint8_t pin_rx, uint8_t pin_tx);
+  my_sds.begin(D7, D6); // begin(uint8_t pin_rx, uint8_t pin_tx);
 
   Serial.begin(115200);
+
+
+  if (EEPROM_ESP8266_READ(192, 224) == "RP")
+  {
+    esid = EEPROM_ESP8266_READ(64, 96);
+    epass = EEPROM_ESP8266_READ(96, 128);
+
+    Serial.println("Установлен режим RP");
+
+    if ( esid.length() > 1 ) {
+      WiFi.begin(esid.c_str(), epass.c_str());
+      if (testWifi(esid)) {
+        Serial.println("Connected to wifi");
+        digitalWrite(LED_BUILTIN, HIGH);
+        printWiFiStatus(); // Print WiFi connection information
+        launchWeb(0);
+        return;
+      }
+    }
+  }
+  ssidAP = EEPROM_ESP8266_READ(128, 160);
+  passwordAP = EEPROM_ESP8266_READ(160, 192);
+  setupAP(true);
+
+  /**
   // Attempt to connect to WiFi network
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
@@ -56,9 +94,192 @@ void setup() {
     WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
     delay(10000);  // Wait 10 seconds to connect
   }
-  Serial.println("Connected to wifi");
-  digitalWrite(LED_BUILTIN, HIGH);
-  printWiFiStatus(); // Print WiFi connection information
+  **/
+}
+
+bool testWifi(String _ssid)
+{
+  int c = 0;
+  Serial.println("Check for WiFi connected...");
+  while ( c < 10 ) {
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Connected to WiFi network - " + _ssid);
+      return true;
+    }
+    delay(4000);
+    //Serial.print(WiFi.status());
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Подключение RP неустановлено.");
+  return false;
+}
+
+void launchWeb(int webtype) {
+  ////Serial.println("");
+  ////Serial.println("WiFi connected");
+  //Serial.print("Local IP: ");
+  //Serial.println(WiFi.localIP());
+  //Serial.print("SoftAP IP: ");
+
+  //Serial.println(WiFi.softAPIP());
+  //Serial.print("SoftAP SSID: ");
+  //Serial.println(ssidAP);
+  createWebServer(webtype);
+  // Start the server
+  _server.begin();
+  //Serial.println("Server started");
+}
+
+void setupAP(bool sta) {
+  if (sta)
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+  }
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+  {
+    //Serial.println("no networks found");
+  }
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li class=ssids onclick=myfunction(this)><a>";
+    st += WiFi.SSID(i) + "</a>";
+    st += " (";
+    st += WiFi.RSSI(i);
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP(ssidAP.c_str(), passwordAP.c_str(), 6);
+  Serial.println("softap");
+  printWiFiStatus();
+  connectedToWiFiSsid = true;
+  launchWeb(1);
+  //Serial.println("over");
+}
+
+void createWebServer(int webtype)
+{
+  if ( webtype == 1 ) {
+    _server.on("/", handleRoot);
+    //clearEepromHttpAnswer = true; // выдать сообщение после очистки памяти EEPROM
+    //server.on("/cleareeprom", clearEeprom);
+    //server.on("/getIP_RPPage", getIP_RPPage);
+
+    //server.on("/changeSsidPage", changeApSsid);
+
+    //server.on("/setup", setupESP);
+    //server.on("/adminAsk", adminAsk);
+    //server.on("/logPassCorrect", logPassCorrect);
+    //server.on("/ask_for_changeAP_RP_mode", ask_for_changeAP_RP_mode);
+    //server.on("/save_changesAP_RP_mode_save", save_changesAP_RP_mode_save);
+    //server.on("/changeAdminPage", ask_for_AdminPass_change);
+    //server.on("/adminlogPassCorrect", adminlogPassCorrect);
+  } else if (webtype == 0) {
+    _server.on("/", []() {
+      IPAddress ip = WiFi.localIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      _server.send(200, "application/json", "{\"IP\":\"" + ipStr + "\"}");
+    });
+  }
+}
+
+void handleRoot()
+{
+  //Arduino:890:5:6:25:60:990
+  Co2_status = "";
+  if (Co2.toFloat() >= 601 && Co2.toFloat() <= 800) {
+    Co2_status = "Yellow";
+    Co2_text_color = "black";
+  }
+  if (Co2.toFloat() >= 801 && Co2.toFloat() <= 900) {
+    Co2_status = "Orange";
+    Co2_text_color = "black";
+  }
+  if (Co2.toFloat() >= 901 && Co2.toFloat() <= 1201) {
+    Co2_status = "Coral";
+    Co2_text_color = "black";
+  }
+  if (Co2.toFloat() >= 1201) {
+    Co2_status = "DarkRed";
+    Co2_text_color = "white";
+  }
+  Serial.print(Co2_status);
+  String file1 =
+    "<!DOCTYPE html>\r\n"
+    "<html>\r\n"
+    "<head>\r\n"
+    "<style>.city{background-color:DarkGreen;color:white}.colorCo2{background-color:" + Co2_status + ";color:" + Co2_text_color + "}table{font-family:arial,sans-serif;border-collapse:collapse;width:60%}td,th{border:2px solid #ddd;text-align:center;padding:15px}tr:nth-child(even){background-color:#ddd}</style>\r\n"
+    "<meta http-equiv=Content-type content=\"text/html; charset=utf-8\" />\r\n"
+    "</head>\r\n"
+    "<body>\r\n"
+    "<h2>Информация с датчиков Co2, PM2.5 и Температура-влажность-давление</h2>\r\n"
+    "<table>\r\n"
+    "<tr class=city>\r\n"
+    "<th width=" + String("40%") + ">Датчик</th>\r\n"
+    "<th width=" + String("70%") + ">Значение</th>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>CO2</td>\r\n"
+    "<td class='colorCo2'>" + Co2 + "</td>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>PM2.5</td>\r\n"
+    "<td class='value'>" + PM2_5 + "</td>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>PM10</td>\r\n"
+    "<td class='value'>" + PM10 + "</td>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>Температура устройства</td>\r\n"
+    "<td class='value'>" + Temp + "</td>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>Влажность устройства</td>\r\n"
+    "<td class='value'>" + Hum + "</td>\r\n"
+    "</tr>\r\n"
+    "<tr>\r\n"
+    "<td>Атмосферное давление</td>\r\n"
+    "<td class='value'>" + Pres + "</td>\r\n"
+    "</tr>\r\n"
+    "</table>\r\n"
+    "</body>\r\n"
+    "</html>\r\n";
+
+  _server.send(200, "text/html", file1);
+}
+
+void handleNotFound()
+{
+  _server.sendHeader("Location", "/", true);
+  _server.send(302, "text/plane", "File Not Find\n\n");
 }
 
 void loop() {
@@ -70,22 +291,26 @@ void loop() {
   {
     indDelaySecs++;
     indDelayWork = 0;
-    //Serial.println("indDelayWork: " + String(indDelaySecs));
+    Serial.println("indDelayWork: " + String(indDelaySecs));
   }
 
-  if (indDelaySecs == 45)
+  if (my_sds_onoff && indDelaySecs == 45)
   {
     my_sds.wakeup();
   }
-  
+
 
   if (indDelaySecs == 60)
   {
+    //showEEPROM();
+    my_sds_onoff = true;
     indDelaySecs = 0;
     error = my_sds.read(&p25, &p10);
     if (! error) {
       Serial.println("P2.5: " + String(p25));
       Serial.println("P10:  " + String(p10));
+      PM2_5 = p25;
+      PM2_5 = p10;
     } else
     {
       Serial.println("P2.5: error");
@@ -107,6 +332,7 @@ void loop() {
       ppm = constrain(ppm, 350, 5000);
     } while (th == 0);
     Serial.println("Co2:" + String(ppm));
+    Co2 = ppm;
 
 
     // If update time has reached 15 seconds, then update the jsonBuffer
@@ -217,4 +443,104 @@ void printWiFiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+
+void showEEPROM()
+{
+  Serial.println("admin login: " + EEPROM_ESP8266_READ(0, 32));
+  Serial.println("admin password: " + EEPROM_ESP8266_READ(32, 64));
+  Serial.println("ssid login: " + EEPROM_ESP8266_READ(64, 96));
+  Serial.println("ssid password: " + EEPROM_ESP8266_READ(96, 128));
+  Serial.println("softAP ssid: " + EEPROM_ESP8266_READ(128, 160));
+  Serial.println("softAP password: " + EEPROM_ESP8266_READ(160, 192));
+  Serial.println("modeAP_RP: " + EEPROM_ESP8266_READ(192, 224));
+}
+
+String EEPROM_ESP8266_READ(int min, int max) {
+  EEPROM.begin(512);
+  delay(10);
+  String buffer;
+  for (int L = min; L < max; ++L)
+    if (isGraph(EEPROM.read(L)))
+      buffer += char(EEPROM.read(L));
+  return buffer;
+}
+
+void writeEEPROM(String adminlogin, String adminpassword, String wifissid, String wifipassword, String softAPName, String softAPPassword, String modeAP_RP)
+{
+  if (adminlogin != "")
+  {
+    clearEepromESP8266(0, 32);
+    EEPROM_ESP8266_WRITE(adminlogin, 0);
+    //Serial.println("admin login changed to " + adminlogin);
+  }
+
+  if (adminpassword != "")
+  {
+    clearEepromESP8266(32, 64);
+    EEPROM_ESP8266_WRITE(adminpassword, 32);
+    //Serial.println("admin password changed to " + adminpassword);
+  }
+
+  if (wifissid != "")
+  {
+    clearEepromESP8266(64, 96);
+    EEPROM_ESP8266_WRITE(wifissid, 64);
+    //Serial.println("ssid login changed to " + wifissid);
+  }
+
+  if (wifipassword != "")
+  {
+    clearEepromESP8266(96, 128);
+    EEPROM_ESP8266_WRITE(wifipassword, 96);
+    //Serial.println("ssid password changed to " + wifipassword);
+  }
+
+  if (softAPName != "")
+  {
+    clearEepromESP8266(128, 160);
+    EEPROM_ESP8266_WRITE(softAPName, 128);
+    //Serial.println("softAP ssid changed to " + softAPName);
+  }
+
+  if (softAPPassword != "")
+  {
+    clearEepromESP8266(160, 192);
+    EEPROM_ESP8266_WRITE(softAPPassword, 160);
+    //Serial.println("softAP password changed to " + softAPPassword);
+  }
+
+  if (modeAP_RP != "")
+  {
+    clearEepromESP8266(192, 224);
+    EEPROM_ESP8266_WRITE(modeAP_RP, 192);
+    //Serial.println("modeAP_RP changed to " + modeAP_RP);
+  }
+
+  //Serial.println("admin login: " + EEPROM_ESP8266_READ(0, 32));
+  //Serial.println("admin password: " + EEPROM_ESP8266_READ(32, 64));
+  //Serial.println("ssid login: " + EEPROM_ESP8266_READ(64, 96));
+  //Serial.println("ssid password: " + EEPROM_ESP8266_READ(96, 128));
+  //Serial.println("softAP ssid: " + EEPROM_ESP8266_READ(128, 160));
+  //Serial.println("softAP password: " + EEPROM_ESP8266_READ(160, 192));
+  //Serial.println("modeAP_RP: " + EEPROM_ESP8266_READ(192, 224));
+}
+
+void clearEepromESP8266(int from, int to)
+{
+  Serial.println("clearing eeprom from " + String(from) + " to " + String(to));
+  for (int i = from; i < to; ++i) {
+    EEPROM.write(i, 0);
+  }
+  EEPROM.commit();
+}
+
+void EEPROM_ESP8266_WRITE(String buffer, int N) {
+  EEPROM.begin(512);
+  delay(10);
+  for (int L = 0; L < 32; ++L) {
+    EEPROM.write(N + L, buffer[L]);
+  }
+  EEPROM.commit();
 }
